2cd50244c5b266f11e41086605c23f2cef0cbe64
yusukekyokawa <medakasuisui598@gmail.com>
2019-11-07 19:55:59 +0900

    画像のリサイズができた

--- utils.py
+++ utils.py
@@ -1,11 +1,36 @@
 import cv2
 import glob
 import os
+from tqdm import tqdm
+
+def resize_image():
+    # リサイズ
+    ROOT = "../型紙データ20171005_ARC"
+    ONE_THIRD_ROOT = "../RESIZE/ONE_THIRD/"
+    QUARTER_ROOT = "../RESIZE/QUARTER/"
+
+    folderlist = glob.glob(ROOT + "/arcKG*")
+    for folder in tqdm(folderlist):
+        foldername = os.path.basename(folder)
+        one_third_folder = os.path.join(ONE_THIRD_ROOT, foldername)
+        quarter_folder = os.path.join(QUARTER_ROOT, foldername)
+        # 画像のフォルダ作成
+        os.makedirs(one_third_folder, exist_ok=True)
+        os.makedirs(quarter_folder, exist_ok=True)
+        pathlist = sorted(glob.glob(folder + "/*.jpg"))
+        for path in pathlist:    
+            filename = os.path.basename(path)
+            ori_img = cv2.imread(path)
+            h, w, c = ori_img.shape[:3]
+            # 1/3
+            one_third_img = cv2.resize(ori_img, (h//3, w//3))
+            # 1/4
+            quarter_img = cv2.resize(ori_img, (h//4, w//4))
+
+            # save image
+            cv2.imwrite(os.path.join(one_third_folder, filename), one_third_img)
+            cv2.imwrite(os.path.join(quarter_folder, filename), quarter_img)
 
 
 if __name__ == "__main__":
-    ROOT = "../型紙データ20171005_ARC"
-
-    folderlist = glob.glob(ROOT + "/*")
-    for path in pathlist:
-    img = cv2.imread()
+    
